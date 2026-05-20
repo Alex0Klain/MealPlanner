b25dad483c8739054e1e4f2230589c6690516f09
Alexey Guz <alex0klain@gmail.com>
2026-05-21 01:12:24 +0300

    Initial Commit

---
+++ MealPlanner/MealPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlannerApp.swift
+//  MealPlanner
+//
+//  Created by Alexey Guz on 21.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct MealPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MealPlannerTests/MealPlannerTests.swift
@@ -0,0 +1,19 @@
+//
+//  MealPlannerTests.swift
+//  MealPlannerTests
+//
+//  Created by Alexey Guz on 21.05.2026.
+//
+
+import Testing
+@testable import MealPlanner
+
+struct MealPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
